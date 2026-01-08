e209eb7e83806adbb09544c2899bfe964431b62d
LamNV - Dev <nguyenvietlam290304@gmail.com>
2026-01-09 00:36:00 +0700

    Valid Parentheses

--- Problems.cs
+++ Problems.cs
@@ -510,6 +510,32 @@ namespace LeetCode
             return dummy.next;
         }
 
+        public bool IsValid(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+            Dictionary<char, char> map = new Dictionary<char, char>
+            {
+                { ')', '(' },
+                { '}', '{' },
+                { ']', '[' }
+            };
+            foreach (char c in s)
+            {
+                if (map.ContainsKey(c))
+                {
+                    char topElement = stack.Count == 0 ? '#' : stack.Pop();
+                    if (topElement != map[c])
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    stack.Push(c);
+                }
+            }
+            return stack.Count == 0;
+        }
         #endregion
 
         //Tip 0ms runtime display =)))))))
