218574fcb6931a258d86a1be2b857135cdd7c3c3
LamNV - Dev <nguyenvietlam290304@gmail.com>
2026-01-11 23:44:45 +0700

    GenerateParenthesis

--- Problems.cs
+++ Problems.cs
@@ -561,7 +561,28 @@ namespace LeetCode
 
             return dummy.next;
         }
-
+        public IList<string> GenerateParenthesis(int n)
+        {
+            IList<string> result = new List<string>();
+            void Backtrack(string current, int open, int close)
+            {
+                if (current.Length == n * 2)
+                {
+                    result.Add(current);
+                    return;
+                }
+                if (open < n)
+                {
+                    Backtrack(current + "(", open + 1, close);
+                }
+                if (close < open)
+                {
+                    Backtrack(current + ")", open, close + 1);
+                }
+            }
+            Backtrack("", 0, 0);
+            return result;
+        }
 
     }
     #endregion
