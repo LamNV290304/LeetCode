2c92445cff1c40fd52a5f690535f7e8fd1117607
LamNV - Dev <nguyenvietlam290304@gmail.com>
2026-01-19 23:33:27 +0700

    RemoveDuplicates

--- Problems.cs
+++ Problems.cs
@@ -700,6 +700,20 @@ namespace LeetCode
 
         public int RemoveDuplicates(int[] nums)
         {
+            if (nums == null || nums.Length == 0) return 0;
+
+            int k = 1;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if ( nums[i] != nums[i - 1])
+                {
+                    nums[i] = nums[i + 1];
+                    k++;
+                }
+            }
+
+            return k;
         }
         #endregion
 
