197aaabb237da77bf41d8c9f5b3ad820c9b80e86
LamNV - Dev <nguyenvietlam290304@gmail.com>
2026-01-19 23:33:57 +0700

    RemoveDuplicates fix

--- Problems.cs
+++ Problems.cs
@@ -708,7 +708,7 @@ namespace LeetCode
             {
                 if ( nums[i] != nums[i - 1])
                 {
-                    nums[i] = nums[i + 1];
+                    nums[k] = nums[i];
                     k++;
                 }
             }
