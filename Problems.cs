8ff4db1ffbbe5577b7fb4df1a317435e099d4cda
LamNV - Dev <nguyenvietlam290304@gmail.com>
2026-01-20 11:16:29 +0700

    StrStr

--- Problems.cs
+++ Problems.cs
@@ -733,6 +733,27 @@ namespace LeetCode
 
             return k;
         }
+
+        public int StrStr(string haystack, string needle)
+        {
+            if (string.IsNullOrEmpty(needle)) return 0;
+            for (int i = 0; i <= haystack.Length - needle.Length; i++)
+            {
+                int j;
+                for (j = 0; j < needle.Length; j++)
+                {
+                    if (haystack[i + j] != needle[j])
+                    {
+                        break;
+                    }
+                }
+                if (j == needle.Length)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
         #endregion
 
         //Tip 0ms runtime display =)))))))
