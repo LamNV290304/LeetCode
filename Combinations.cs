84e026031796f96d3a973ea9a7de8386e5c846d2
LamNV - Dev <nguyenvietlam290304@gmail.com>
2026-01-29 18:21:13 +0700

    Undo

--- Combinations.cs
+++ Combinations.cs
@@ -10,7 +10,18 @@ namespace LeetCode
     {
         public IList<string> LetterCombinations(string digits)
         {
-         
+            Dictionary<char, string> phoneMap = new Dictionary<char, string>()
+            {
+                {'2', "abc"},
+                {'3', "def"},
+                {'4', "ghi"},
+                {'5', "jkl"},
+                {'6', "mno"},
+                {'7', "pqrs"},
+                {'8', "tuv"},
+                {'9', "wxyz"}
+            };
+
             List<string> result = new List<string>();
 
             foreach (char digit in digits)
