62819d7e164e83e2399ada5b8737825fa23cb5d2
LamNV - Dev <nguyenvietlam290304@gmail.com>
2026-01-31 21:15:54 +0700

    no

--- Problems.cs
+++ Problems.cs
@@ -993,42 +993,42 @@ namespace LeetCode
             return;
         }
 
-            public int SearchInRotatedSortedArray(int[] nums, int target)
+        public int SearchInRotatedSortedArray(int[] nums, int target)
+        {
+            int left = 0;
+            int right = nums.Length - 1;
+            while (left <= right)
             {
-                int left = 0;
-                int right = nums.Length - 1;
-                while (left <= right)
+                int mid = left + (right - left) / 2;
+                if (nums[mid] == target)
                 {
-                    int mid = left + (right - left) / 2;
-                    if (nums[mid] == target)
+                    return mid;
+                }
+                else if (nums[left] <= nums[mid])
+                {
+                    if (nums[left] <= target && target < nums[mid])
                     {
-                        return mid;
-                    }
-                    if (nums[left] <= nums[mid])
-                    {
-                        if (nums[left] <= target && target < nums[mid])
-                        {
-                            right = mid - 1;
-                        }
-                        else
-                        {
-                            left = mid + 1;
-                        }
+                        right = mid - 1;
                     }
                     else
                     {
-                        if (nums[mid] < target && target <= nums[right])
-                        {
-                            left = mid + 1;
-                        }
-                        else
-                        {
-                            right = mid - 1;
-                        }
+                        left = mid + 1;
                     }
                 }
-                return -1;
+                else
+                {
+                    if (nums[mid] < target && target <= nums[right])
+                    {
+                        left = mid + 1;
+                    }
+                    else
+                    {
+                        right = mid - 1;
+                    }
+                }
             }
+            return -1;
+        }
 
         #endregion
 
